22ab271b323b552e8ddeebc78f214813963c1dd7
Magnus Furugård <mafur90@gmail.com>
2020-10-25 12:11:27 +0100

    working infinite app

--- flink-cluster/DummyApp/src/main/scala/dummyApp/StreamingJob.scala
+++ flink-cluster/DummyApp/src/main/scala/dummyApp/StreamingJob.scala
@@ -1,14 +1,56 @@
+/*
+ * Example copied from here:
+ * https://github.com/apache/flink/blob/master/flink-examples/flink-examples-streaming/src/main/scala/org/apache/flink/streaming/scala/examples/async/AsyncIOExample.scala
+ */
 package dummyApp
 
+import java.util.concurrent.TimeUnit
+
+import org.apache.flink.streaming.api.functions.source.ParallelSourceFunction
+import org.apache.flink.streaming.api.functions.source.SourceFunction.SourceContext
 import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.api.scala.async.ResultFuture
+
+import scala.concurrent.{ExecutionContext, Future}
 
 object StreamingJob {
+
   def main(args: Array[String]) {
+    val timeout = 10000L
+
     val env = StreamExecutionEnvironment.getExecutionEnvironment
-    env.fromCollection(List(0).map(x => {
-      println(s"Sleeping for ${Long.MaxValue}ms")
-      Thread.sleep(Long.MaxValue)
-    }))
-    env.execute("Example app - infinite no-op stream")
+
+    val input = env.addSource(new SimpleSource())
+
+    val asyncMapped = AsyncDataStream.orderedWait(input, timeout, TimeUnit.MILLISECONDS, 10) {
+      (input, collector: ResultFuture[Int]) =>
+        Future {
+          collector.complete(Seq(input))
+        } (ExecutionContext.global)
+    }
+
+    asyncMapped.print()
+
+    env.execute("Example app - Infinite counter")
+  }
+}
+
+class SimpleSource extends ParallelSourceFunction[Int] {
+  var running = true
+  var counter = 0
+
+  override def run(ctx: SourceContext[Int]): Unit = {
+    while (running) {
+      ctx.getCheckpointLock.synchronized {
+        ctx.collect(counter)
+      }
+      counter += 1
+
+      Thread.sleep(1000L)
+    }
+  }
+
+  override def cancel(): Unit = {
+    running = false
   }
 }
